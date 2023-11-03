5930d381f0fc14972eef14acd5792c9c820ccbea
mairuu <kmr.mairuu@gmail.com>
2023-11-03 14:25:40 +0700

    hint for search page

--- src/routes/(main)/search/+page.svelte
+++ src/routes/(main)/search/+page.svelte
@@ -105,13 +105,10 @@
 
 	<div class="mx-4 my-6 md:mx-0">
 		<div class="flex gap-4">
-			<label
-				for="search-input"
-				class="flex flex-1 items-center rounded-md bg-base-200 outline-2 focus-within:outline"
-			>
+			<label for="search-input" class="input input-bordered flex w-full items-center">
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
-					class="mx-3 h-6 w-6"
+					class="mr-3 h-6 w-6"
 					viewBox="0 0 24 24"
 					stroke-width="2"
 					stroke="currentColor"
@@ -141,9 +138,27 @@
 					}}
 				/>
 			</label>
-		</div>
 
-		<button class="btn btn-sm mt-4 w-full" on:click={showFilterModal}>filter</button>
+			<button class="btn btn-circle btn-ghost" title="search filter" on:click={showFilterModal}
+				><svg
+					xmlns="http://www.w3.org/2000/svg"
+					class="icon icon-tabler icon-tabler-filter"
+					width="24"
+					height="24"
+					viewBox="0 0 24 24"
+					stroke-width="2"
+					stroke="currentColor"
+					fill="none"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+				>
+					<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+					<path
+						d="M4 4h16v2.172a2 2 0 0 1 -.586 1.414l-4.414 4.414v7l-6 2v-8.5l-4.48 -4.928a2 2 0 0 1 -.52 -1.345v-2.227z"
+					></path>
+				</svg></button
+			>
+		</div>
 
 		<dialog bind:this={filterDialog} class="modal modal-bottom sm:modal-middle">
 			<form method="dialog" class="modal-box">
@@ -201,6 +216,11 @@
 			</div>
 		{/await}
 	{:else}
-		<div class="mx-4 my-6 md:mx-0">hint</div>
+			<div class="mx-4 my-6 md:mx-0">
+				<div class="flex flex-col items-center pt-[20vh]">
+					<span class="inline-block text-4xl"> (＾▽＾) </span>
+					<span class="pt-4"> Fill in keywords, Enter, Explore! </span>
+				</div>
+			</div>
 	{/if}
 </div>
